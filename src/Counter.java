11ad44a63945a27b45469eb4ebe66ddc09ed5dd9
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-02-17 22:23:37 +0200

    method not working?

--- src/Counter.java
+++ src/Counter.java
@@ -1,9 +1,9 @@
 public class Counter {
     int value;
-    void increment() {
+    synchronized void increment() {
         value = value+1;
     }
-    void decrement() {
+    synchronized void decrement() {
         value = value-1;
     }
     int getCount() {
