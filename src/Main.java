5cc788743b52b9e30769a63933b8a9d91cf9d423
Paul-Vasylenko <maincrafter1307@gmail.com>
2023-03-03 14:36:00 +0200

    add wrong implementation for example

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         try {
             for(int i=0; i<100; i++){
-                SharedTurn turn = new SharedTurn();
-                PrintThread thread1 = new PrintThread("-", turn);
-                PrintThread thread2 = new PrintThread("|", turn);
+                PrintThread thread1 = new PrintThread("-");
+                PrintThread thread2 = new PrintThread("|");
                 thread1.start();
                 thread2.start();
 
--- src/PrintThread.java
+++ src/PrintThread.java
@@ -1,21 +1,13 @@
 class PrintThread extends Thread {
     private String symbol;
-    private SharedTurn turn;
 
-    public PrintThread(String symbol, SharedTurn turn) {
+    public PrintThread(String symbol) {
         this.symbol = symbol;
-        this.turn = turn;
     }
 
     public void run() {
         for (int i = 0; i < 50; i++) {
-            try {
-                turn.waitForTurn(symbol);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             System.out.print(symbol);
-            turn.nextTurn();
         }
     }
 }
